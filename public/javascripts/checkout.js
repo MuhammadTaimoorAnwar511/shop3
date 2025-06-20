074e93b2f130967ceb7324bfc03723c16c27f5b3
Muhammad Taimoor Anwar <129211565+MuhammadTaimoorAnwar511@users.noreply.github.com>
2025-06-20 18:00:45 +0500

    Update checkout.js

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -1,5 +1,5 @@
 // Create a Stripe client.
-const stripe = Stripe("pk_test_Nw7zXh6zu9SXKrzk7KDxKUiV004Ly59ywq");
+const stripe = Stripe("pk_test_51Rc4LmRszeFeau3wfXkwwbvkSJmirN0gERggJYBVxBDLg6IT35FZF5MJIyFNRSKFrUMKx1LOFBQiVqpFPkKgt5zq00oIrJchZn");
 
 // Create an instance of Elements.
 const elements = stripe.elements();
